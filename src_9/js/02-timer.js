18f927b5fb6f14ee57a2f11c626dc3e9079ab0ba
Oleh Borys <olv.borys@gmail.com>
2023-10-23 09:44:39 +0200

    func

--- src_9/js/02-timer.js
+++ src_9/js/02-timer.js
@@ -1,1 +1,12 @@
-const cust = false;
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
