41221aa65a93415683508923c8001cf52939e32a
Oleh Borys <olv.borys@gmail.com>
2024-02-20 10:54:04 +0100

    refresh

---
+++ src_11/src/js/fetchImages.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { KEY } from "./api-key.js";
+
+//checking for the mistakes during request to the server
+axios.defaults.baseURL = "https://pixabay.com/api/";
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    Notiflix.Notify.failure("Something went wrong. Please try again later.");
+    return Promise.reject(error);
+  }
+);
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response.data;
+}
+export { fetchImages };
